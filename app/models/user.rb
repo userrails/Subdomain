c509fee176dcad23159c8773bff01f0f51935bb4
rails <rails@rails>
2014-12-11 21:23:24 +0530

    small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  # validates_format_of :subdomain, with: /^[a-z0-9_]+$/, message: "must be lowercase alphanumerics only"
-  # validates_length_of :subdomain, maximum: 32, message: "exceeds maximum of 32 characters"
-  # validates_exclusion_of :subdomain, in: ['www', 'mail', 'ftp'], message: "is not available"
-
-  validates :subdomain, :uniqueness => true
-
+  validates_format_of :subdomain, with: /\A[a-z0-9_]+\Z/, message: "must be lowercase alphanumerics only"
+  validates_length_of :subdomain, maximum: 32, message: "exceeds maximum of 32 characters"
+  validates_exclusion_of :subdomain, in: ['www', 'mail', 'ftp'], message: "is not available"
+  validates :subdomain, :uniqueness => true, :presence => true
   has_many :products
 end
